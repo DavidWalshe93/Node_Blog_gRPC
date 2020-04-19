091df00155e316fcad723fe8594fa12680bbd0af
david.walshe93@gmail.com <david.walshe93@gmail.com>
2020-04-19 22:19:41 +0100

    Added boilerplate server code for grpc.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,26 @@
 // Created by David Walshe on 19/04/2020
 
+// NPM imports
+const grpc = require("grpc");
+
+// Proto imports
+const blog = require("../proto_out/proto/blog_pb");
+const service = require("../proto_out/proto/blog_grpc_pb");
+
 // KNEX Setup
 const environment = process.env.ENVIRONMENT || "development";
 const config = require("./knexfile")[environment];
 const knew = require("knex")(config);
+
+const main = () => {
+    const server = new grpc.Server();
+
+    server.addService(service.BlogServiceService, {});
+
+    server.bind("127.0.0.1:50051", grpc.ServerCredentials.createInsecure());
+    server.start();
+
+    console.log("Server running on 127.0.0.1:50051")
+};
+
+main();
