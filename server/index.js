4642ed72b21223dc0efdfe00453575cbf2b9815c
david.walshe93@gmail.com <david.walshe93@gmail.com>
2020-04-19 22:58:06 +0100

    Added RPC for creating a blog on the server.

--- server/index.js
+++ server/index.js
@@ -12,6 +12,38 @@ const environment = process.env.ENVIRONMENT || "development";
 const config = require("./knexfile")[environment];
 const knex = require("knex")(config);
 
+
+const createBlog = (call, cb) => {
+    const blog = call.request.getBlog();
+
+    knex("blogs").insert({
+        author: blog.getAuthor(),
+        title: blog.getTitle(),
+        content: blog.getContent()
+    }).then(() => {
+        let id = blog.getId();
+        let addedBlog = new blogs.Blog();
+
+        // Set the blog response to be returned
+        addedBlog.setId(id);
+        addedBlog.setAuthor(blog.getAuthor());
+        addedBlog.setTitle(blog.getTitle());
+        addedBlog.setContent(blog.getContent());
+
+        // Create a protobuf response object.
+        let blogResponse = new blogs.CreateBlogResponse();
+
+        // Add the blog message to the response.
+        blogResponse.setBlog(addedBlog);
+
+        console.log("Inserted Blog with ID: ", blogResponse.getId());
+
+        // Send response to client
+        cb(null, blogResponse)
+    })
+};
+
+
 const listBlog = (call, cb) => {
     // Retrieve data from the Postgres database via a Promise.
     knex("blogs").then((data) => {
@@ -39,8 +71,10 @@ const listBlog = (call, cb) => {
 const main = () => {
     const server = new grpc.Server();
 
+    // Added RPC API Services to Server.
     server.addService(service.BlogServiceService, {
-        listBlog: listBlog
+        createBlog: createBlog,
+        listBlog: listBlog,
     });
 
     server.bind("127.0.0.1:50051", grpc.ServerCredentials.createInsecure());
